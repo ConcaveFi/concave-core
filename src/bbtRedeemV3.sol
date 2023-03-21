9bd1ce0c8e17b215197227f838d68a7d87807a67
concave-engineering <97050605+concave-engineering@users.noreply.github.com>
2023-03-21 14:36:11 -0400

    🐛 Redundant redemption accounting

--- src/bbtRedeemV3.sol
+++ src/bbtRedeemV3.sol
@@ -35,12 +35,16 @@ contract RedeemBBTV3 is Owned {
     /// @notice address of RedeemBBT V1
     address public immutable redeemBBTV1;
 
+    address public immutable redeemBBTV2;
+
     /// @notice address of staking contract
     address public immutable staking;
 
     uint256 public immutable deadline;
+
     /// @notice mapping of how many CNV tokens a bbtCNV holder has redeemed
     mapping(address => uint256) public redeemed;
+    
     /// @notice redeem paused;
     bool public paused;
 
@@ -57,12 +61,14 @@ contract RedeemBBTV3 is Owned {
         address _bbtCNV, 
         address _CNV, 
         address _redeemBBTV1,
+        address _redeemBBTV2,
         address _staking,
         uint256 _deadline
     ) Owned(msg.sender) {
         bbtCNV = _bbtCNV;
         CNV = _CNV;
         redeemBBTV1 = _redeemBBTV1;
+        redeemBBTV2 = _redeemBBTV2;
         staking = _staking;
         deadline = _deadline;
     }
@@ -91,38 +97,34 @@ contract RedeemBBTV3 is Owned {
     function redeem(
         uint256 _amount, 
         address _to, 
-        bool _max,
         bool _stake,
         uint256 _poolId
     ) external returns (uint256 amountOut) {
+        // still need to write tests
+        // predeadline post deadline stuff
         require(!paused, "PAUSED");
-        require(block.timestamp < deadline, "PAST DEADLINE");
 
         uint256 bbtCNVBalance = IERC20(bbtCNV).balanceOf(msg.sender);
 
-        // Check v1/v2 redemption amounts, aggregate with address(this)
         uint256 amountRedeemed = redeemed[msg.sender] + IRedeemBBT(redeemBBTV1).redeemed(msg.sender) + IRedeemBBT(redeemBBTV2).redeemed(msg.sender);
 
         require(bbtCNVBalance > amountRedeemed, NONE);
 
         uint256 amountRedeemable = bbtCNVBalance - amountRedeemed;
-        amountOut = amountRedeemable;
-        if (!_max) {
-            require(amountRedeemable >= _amount,"EXCEEDS");
-            amountOut = _amount;
-        }
 
-        redeemed[msg.sender] += amountOut;
+        require(amountRedeemable >= _amount,"EXCEEDS");
+        
+        redeemed[msg.sender] += amountRedeemable;
 
-       if (_stake) {
-            // they decide to stake so no need to transfer
-            IStaking(staking).stake(amountOut, _poolId);
+       if (_stake && block.timestamp < deadline) {
+        IStaking(staking).lock(_to, amountRedeemable, _poolId);
+        } else if (_stake && block.timestamp >= deadline) {
+        IStaking(staking).lock(_to, amountRedeemable, 0);
         } else {
-            // Else its a firesale at a psm ratio
-            ICNV(CNV).transfer(_to, amountOut * 1e18 / rate);
-            emit Redemption(msg.sender, amountOut);
+        ICNV(CNV).transfer(_to, amountRedeemable * 1e18 / rate);
         }
-
+        
+        emit Redemption(msg.sender, amountRedeemable);
     }
 
 }
